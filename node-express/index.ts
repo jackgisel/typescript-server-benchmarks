66f9df7b2936c353619e35cacc1fee65e78ac0d2
Jack G <jackrgisel@gmail.com>
2023-07-16 07:15:36 -0700

    added prisma connection

--- node-express/index.ts
+++ node-express/index.ts
@@ -1,10 +1,31 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
+});
+
+app.get("/tasks", async (req: express.Request, res: express.Response) => {
+  const tasks = await prisma.task
+    .findMany()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+  res.send({
+    data: {
+      tasks: tasks,
+    },
+  });
 });
 
 app.listen(PORT, () => {
