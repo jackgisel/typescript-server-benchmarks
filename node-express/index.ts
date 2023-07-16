0646debd4c0839a89b25df5fc72347b95830f633
Jack G <jackrgisel@gmail.com>
2023-07-16 07:42:58 -0700

    added a seeding script

--- node-express/index.ts
+++ node-express/index.ts
@@ -1,26 +1,28 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 
+require("dotenv").config();
 const prisma = new PrismaClient();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8888;
+
+app.use("", (req: express.Request, res: express.Response, next) => {
+  console.log("Incoming request to ", req.baseUrl);
+  next();
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
 
 app.get("/tasks", async (req: express.Request, res: express.Response) => {
-  const tasks = await prisma.task
-    .findMany()
-    .then(async () => {
-      await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-      console.error(e);
-      await prisma.$disconnect();
-      process.exit(1);
-    });
+  const tasks = await prisma.task.findMany({
+    include: {
+      owner: true,
+    },
+  });
   res.send({
     data: {
       tasks: tasks,
